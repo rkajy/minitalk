31f4cb77649441b5f01077abd830a8ad28edbc2b
rkajy <rado_andria@yahoo.com>
2025-09-16 21:15:57 +0200

    add necessary librairy and handle errors in client function for incorect number of parameters

--- client.c
+++ client.c
@@ -6,15 +6,31 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 09:30:34 by radandri          #+#    #+#             */
-/*   Updated: 2025/09/16 10:48:23 by radandri         ###   ########.fr       */
+/*   Updated: 2025/09/16 21:11:36 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void    client(int server_pid, char* message[])
+{
+    (void) server_pid;
+    (void) message;
+}
+
 int main(int argc, char* argv[])
 {
-    (void) argc;
-    (void) argv;
+    int server_pid;
+    if(argc == 3)
+    {
+        server_pid = atoi(argv[1]);
+        (void) server_pid;
+    }
+    else{
+        ft_printf("Error: Invalid input\n");
+        ft_printf("Usage: %s <server_pid> <message_to_send_to_server>\n", argv[0]);
+        ft_printf("Eg: %s 12354 \"Hello world\"\n", argv[0]);
+        return (1);
+    }
     return 0;
 }
--- minitalk.h
+++ minitalk.h
@@ -6,12 +6,16 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 09:30:19 by radandri          #+#    #+#             */
-/*   Updated: 2025/09/16 11:21:24 by radandri         ###   ########.fr       */
+/*   Updated: 2025/09/16 21:11:24 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 # include "libftprintf/ft_printf.h"
+#include <unistd.h> // write, getpid, pause, sleep, usleep
+#include <signal.h> // signal, sigemptyset, sigaddset, sigaction, kill
+#include <stdlib.h> // malloc, free, exit, atoi
+
 
 #endif
