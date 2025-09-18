0cf6b53ca39b2314cac77c87b06007a1e9f70975
rkajy <rado_andria@yahoo.com>
2025-09-18 19:42:22 +0200

    add function string to binary

--- client.c
+++ client.c
@@ -6,25 +6,85 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 09:30:34 by radandri          #+#    #+#             */
-/*   Updated: 2025/09/16 21:11:36 by radandri         ###   ########.fr       */
+/*   Updated: 2025/09/18 17:02:07 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    client(int server_pid, char* message[])
+void print_binary_array(char* binary, size_t size)
 {
-    (void) server_pid;
-    (void) message;
+    for (size_t i = 0; i < size; i++)
+    {
+        ft_printf("%d", binary[i]);
+    }
+    ft_printf("\n");
+}
+
+char* stringMessageToBinary(char message[])
+{
+    int i;
+    int j;
+    int k;
+    char* binary;
+
+    binary = malloc(ft_strlen(message) * 8);
+    if (!binary)
+        return (NULL);
+    
+    j=0;
+    k = 0;
+    while (message[j] != '\0')
+    {
+        i=7;
+        while (i >= 0)
+        {
+            binary[k] = (message[j] >> i) & 1;
+            i--;
+            k++;
+        }
+        j++;
+    }
+    binary[k] = '\0';
+    //print_binary_array(binary, ft_strlen(message) * 8);
+    return (binary);
+}
+
+void    send_client_request_to_server(int server_pid, char message[])
+{
+    int i;
+    char* binary;
+    int binary_size;
+
+    ft_printf("Information from client, pid: %d and message to send: \"%s\"\n", server_pid, message);
+    
+    i=0;
+    binary = stringMessageToBinary(message);
+    binary_size = ft_strlen(message) * 8;
+    while (i < binary_size)
+    {
+        //send
+        if(binary[i] == '0')
+            kill(server_pid, SIGUSR1);
+        else if(binary[i] == '1')
+            kill(server_pid, SIGUSR2);
+        i++;
+    }
+    
 }
 
 int main(int argc, char* argv[])
 {
     int server_pid;
+    char *binary;
     if(argc == 3)
     {
         server_pid = atoi(argv[1]);
-        (void) server_pid;
+        send_client_request_to_server(server_pid, argv[2]);
+        binary = stringMessageToBinary("A");
+
+        // don't forget for memory leaks issue
+        free(binary);
     }
     else{
         ft_printf("Error: Invalid input\n");
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 09:30:46 by radandri          #+#    #+#             */
-/*   Updated: 2025/09/16 10:47:57 by radandri         ###   ########.fr       */
+/*   Updated: 2025/09/16 22:01:51 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int main(int argc, char* argv[])
 {
+    pid_t pid;
+    
+    pid = getpid();
+    ft_printf("The server process ID is %d\n", pid);
     (void) argc;
     (void) argv;
+    (void) pid;
     return 0;
 }
